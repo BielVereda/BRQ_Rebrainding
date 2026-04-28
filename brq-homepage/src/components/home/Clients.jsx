861a3d9986e64cedd393b75eecb291cd1bccc27d
BielVereda <gabrielsantosvereda@gmail.com>
2026-04-28 11:10:10 -0300

    refactor: Change the ways for images of clients

--- brq-homepage/src/components/home/Clients.jsx
+++ brq-homepage/src/components/home/Clients.jsx
@@ -2,24 +2,24 @@ import { useState, useEffect } from "react";
 import "../../styles/home/clients.scss";
 
 const clientsList = [
-    { src: "/assets/images/logo_ab-inbev.png", alt: "AB InBev" },
-    { src: "/assets/images/logo_alelo.png", alt: "Alelo" },
-    { src: "/assets/images/logo_b3.png", alt: "B3" },
-    { src: "/assets/images/logo_btg-pactual.png", alt: "BTG Pactual" },
-    { src: "/assets/images/logo_cpfl.png", alt: "CPFL" },
-    { src: "/assets/images/logo_electrolux.png", alt: "Electrolux" },
-    { src: "/assets/images/logo_gol.png", alt: "Gol" },
-    { src: "/assets/images/logo_itau.png", alt: "Itaú" },
-    { src: "/assets/images/logo_nissan.png", alt: "Nissan" },
-    { src: "/assets/images/logo_novo-nordisk.png", alt: "Novo Nordisk"},
-    { src: "/assets/images/logo_pfizer.png", alt: "Pfizer" },
-    { src: "/assets/images/logo_porto.png", alt: "Porto" },
-    { src: "/assets/images/logo_rd-saude.png", alt: "RD Saúde"},
-    { src: "/assets/images/logo_santander.png", alt: "Santander" },
-    { src: "/assets/images/logo_syngenta.png", alt: "Syngenta" },
-    { src: "/assets/images/logo_vibra.png", alt: "Vibra" },
-    { src: "/assets/images/logo_visa.png", alt: "Visa" },
-    { src: "/assets/images/logo_vitru.png", alt: "Vitru" },
+    { src: "/assets/images/companies/logo_ab-inbev.png", alt: "AB InBev" },
+    { src: "/assets/images/companies/logo_alelo.png", alt: "Alelo" },
+    { src: "/assets/images/companies/logo_b3.png", alt: "B3" },
+    { src: "/assets/images/companies/logo_btg-pactual.png", alt: "BTG Pactual" },
+    { src: "/assets/images/companies/logo_cpfl.png", alt: "CPFL" },
+    { src: "/assets/images/companies/logo_electrolux.png", alt: "Electrolux" },
+    { src: "/assets/images/companies/logo_gol.png", alt: "Gol" },
+    { src: "/assets/images/companies/logo_itau.png", alt: "Itaú" },
+    { src: "/assets/images/companies/logo_nissan.png", alt: "Nissan" },
+    { src: "/assets/images/companies/logo_novo-nordisk.png", alt: "Novo Nordisk"},
+    { src: "/assets/images/companies/logo_pfizer.png", alt: "Pfizer" },
+    { src: "/assets/images/companies/logo_porto.png", alt: "Porto" },
+    { src: "/assets/images/companies/logo_rd-saude.png", alt: "RD Saúde"},
+    { src: "/assets/images/companies/logo_santander.png", alt: "Santander" },
+    { src: "/assets/images/companies/logo_syngenta.png", alt: "Syngenta" },
+    { src: "/assets/images/companies/logo_vibra.png", alt: "Vibra" },
+    { src: "/assets/images/companies/logo_visa.png", alt: "Visa" },
+    { src: "/assets/images/companies/logo_vitru.png", alt: "Vitru" }
 ];
 
 // divide em grupos de 6
